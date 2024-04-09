03a30a810f8d318277476dd7e12c89b8225d6a16
johndaniell@yahoo.com <johndaniell@yahoo.com>
2024-04-09 17:13:07 +0200

    fixed sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,15 +1,19 @@
 import { Link } from "react-router-dom";
+import styles from './Sidebar.module.css';
 
 const Sidebar = ({ newItems, markItemsAsOld }) => {
   // You could call markItemsAsOld at some point to reset the isNew flags
 
   return (
-    <div className="sidebar">
+    <div className={styles.sidebar}>
       <h3>Newly Added Items</h3>
       <ul>
         {newItems?.map((item) => (
-          <Link to={`/items/${item.id}`} >
-          <li key={item.id}>{item.name}</li>
+          <Link to={`/items/${item.id}`} key={item.id} className={styles.link}>
+            <li className={styles.item}>
+              <img src={item.picture_url.url} alt={item.name} className={styles.img} />
+              <span>{item.name}</span>
+            </li>
           </Link>
         ))}
       </ul>
