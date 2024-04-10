0057ba0bfe97df7c68441738153a3f34701e4dfc
dieegoolimaa <dieegoolimaa@gmail.com>
2024-04-10 08:53:16 +0100

    add logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,14 @@
-
-import { Link } from 'react-router-dom';
-import styles from './Navbar.module.css'; // Make sure to use the correct path
+import { Link } from "react-router-dom";
+import styles from "./Navbar.module.css";
+import logo from "./image/LOGOSITE.png";
 
 function Navbar() {
   return (
     <nav className={styles.navbar}>
-      <Link to="/" className={styles.appName}>Apartment Rentals</Link>
+      <img src={logo} alt="Logo" style={{ width: "60px" }} />
+      <Link to="/" className={styles.appName}>
+        Apartment Rentals
+      </Link>
       <div className={styles.navigationLinks}>
         <Link to="/">Dashboard</Link>
         <Link to="/about">About</Link>
